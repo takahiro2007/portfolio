03137ddfb3950841423d574c4521a8861d8370fd
takahiro2007 <watataka2007728@gmail.com>
2023-01-20 15:09:28 +0900

    cording at header

--- components/header.tsx
+++ components/header.tsx
@@ -3,13 +3,23 @@ import { css } from "@emotion/react";
 const Header = () => {
   return (
     <div css={headerContent}>
-      <h1>Takahiro Watanabe</h1>
+      <h1 css={headerTitle}>Takahiro Watanabe</h1>
+      <div></div>
     </div>
   );
 };
+//ダークモードの時は、読み込むロゴのファイルを変える
 
 const headerContent = css`
-  font-weight: normal;
+  background: red;
+  max-width: 960px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const headerTitle = css`
+  font-weight: 100;
+  font-size: 40px;
 `;
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["100", "300"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
